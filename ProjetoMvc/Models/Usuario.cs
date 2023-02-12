eedc5a35c0dc3041a3c358a3f74e0629c7129797
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-02-12 14:08:05 -0300

    Segunda tentativa para o projeto de Cadastro de usuário

---
+++ ProjetoMvc/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoMvc.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name ="Codigo")]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+        [Display(Name = "Email")]
+        [Column("Email")]
+        public string Email { get; set; }
+        [Display(Name = "Telefone")]
+        [Column("Telefone")]
+        public string Telefone { get; set; }
+    }
+}
